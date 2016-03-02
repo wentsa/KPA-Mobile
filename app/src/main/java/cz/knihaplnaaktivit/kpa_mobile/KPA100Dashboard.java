06fddeffb121d2c840f93f4903fe218786bc5757
Tomas Chalupnik <wentsa@seznam.cz>
2016-03-03 00:32:16 +0100

    dashboard

--- app/src/main/java/cz/knihaplnaaktivit/kpa_mobile/KPA100Dashboard.java
+++ app/src/main/java/cz/knihaplnaaktivit/kpa_mobile/KPA100Dashboard.java
@@ -1,7 +1,11 @@
 package cz.knihaplnaaktivit.kpa_mobile;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class KPA100Dashboard extends AppCompatActivity {
 
@@ -9,5 +13,37 @@ public class KPA100Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kpa100_dashboard);
+    }
+
+    public void onBtnSummaryClicked(View v) {
+
+    }
+
+    public void onBtnContactUsClicked(View v) {
+
+    }
+
+    public void onBtnSharePhotoClicked(View v) {
+
+    }
+
+    public void onBtnAboutClicked(View v) {
+
+    }
+
+    public void onBtnFacebookClicked(View v) {
+        String facebookUrl = "https://www.facebook.com/www.knihaplnaaktivit.cz";
+        String facebookId = "1552685675006861";
+        try {
+            int versionCode = getPackageManager().getPackageInfo("com.facebook.katana", 0).versionCode;
+            if (versionCode >= 3002850) {
+                Uri uri = Uri.parse("fb://facewebmodal/f?href=" + facebookUrl);
+                startActivity(new Intent(Intent.ACTION_VIEW, uri));
+            } else {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/" + facebookId)));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(facebookUrl)));
+        }
     }
 }
