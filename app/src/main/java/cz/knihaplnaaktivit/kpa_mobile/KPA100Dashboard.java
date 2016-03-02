367c120f902de385d0fbdf9e62a9177fcda70f9e
Tomas Chalupnik <wentsa@seznam.cz>
2016-03-02 23:20:06 +0100

    initial commit

---
+++ app/src/main/java/cz/knihaplnaaktivit/kpa_mobile/KPA100Dashboard.java
@@ -0,0 +1,13 @@
+package cz.knihaplnaaktivit.kpa_mobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KPA100Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kpa100_dashboard);
+    }
+}
